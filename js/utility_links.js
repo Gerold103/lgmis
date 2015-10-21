5d006c6b0909bebaac57d66d3ac8c97f64334e5b
Gerold103 <vshpilevoi@mail.ru>
2015-10-22 01:53:48 +0300

    local changes

--- js/utility_links.js
+++ js/utility_links.js
@@ -1,4 +1,4 @@
-var link_prefix = '/lgmis2/lgmis/';
+var link_prefix = '/';
 var link_to_utility_language_change = 'utility_language_change.php';
 var link_to_admin_ajax_interceptor = 'admin_ajax_interceptor.php';
 
--- test2.php
+++ test2.php
@@ -1,6 +1,9 @@
 <?php
 	require_once('utility_lgmis_lib.php');
-
+	// $password = password_hash('123456', PASSWORD_DEFAULT);
+	// $res = $db_connection->query("UPDATE users SET password = '".$password."' WHERE id = 21");
+	// if ($res) echo 'ok';
+	// else echo 'error';
 	//move_uploaded_file($_FILES['file']['tmp_name'], 'files/'.urldecode($_FILES['file']['name']));
 	//file_put_contents('files/[debug].txt', grab_dump($_FILES)."\xA");
 
